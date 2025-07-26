f8e33c1daecc0c2b4382a99c93b355dc8e0d07a0
leofitnees8 <leofitnees8@gmail.com>
2025-07-27 00:00:22 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHwhmB5ppbyHNR7gUaDnybG4e9nAhAkg",
-  authDomain: "leoo-89ed3.firebaseapp.com",
-  projectId: "leoo-89ed3",
-  storageBucket: "leoo-89ed3.appspot.com",
-  messagingSenderId: "615783098059",
-  appId: "1:615783098059:web:bc06574a9282633cff288e",
-  measurementId: "G-JWQ7YN2V9Q"
+  apiKey: "AIzaSyDnES93NzE5TGxtyqZ5xW-I78IExV1f1eE",
+  authDomain: "ems-282f0.firebaseapp.com",
+  projectId: "ems-282f0",
+  storageBucket: "ems-282f0.appspot.com",
+  messagingSenderId: "33075424116",
+  appId: "1:33075424116:web:7578b6c965280f450ada21",
+  measurementId: "G-NG7KRQEUJB"
 };
 
 const app = initializeApp(firebaseConfig);
